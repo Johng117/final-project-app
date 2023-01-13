155c3498d7508e91ef8fef480b4ba0cfbb1a4155
Johng117 <83654331+Johng117@users.noreply.github.com>
2023-01-13 18:49:42 +0000

    Updates to App.js and Quote.js

--- src/App.js
+++ src/App.js
@@ -4,40 +4,37 @@ import Button from "./components/Button/Button";
 // import quoteList from "./app-quote-data.json";
 import "materialize-css/dist/css/materialize.min.css";
 
-
 function App() {
   const [isClicked, setIsClicked] = useState(false);
   const [quote, setQuote] = useState({});
 
-// function to handle click event and update state of isClicked
+  // function to handle click event and update state of isClicked
   const handleClick = () => {
     setIsClicked(!isClicked);
   };
 
-// function that creates a random number and updates setQuote with another quote that has the index of the random number
+  // function that creates a random number and updates setQuote with another quote that has the index of the random number
   const ranQuote = () => {
-      // let randomNumber = Math.floor(Math.random()*quoteList.length)
-      // console.log("random", randomNumber)
-      // setQuote(quoteList[randomNumber])
-      fetch("http://10.0.40.63:3001/quote")
-        .then(function (response) {
-          if (response.ok) {
-            return response.json();
-          }
-          throw `${response.status} ${response.statusText}`;
-        })
-        .then(function (quote) {
-          setQuote(quote);
-        })
-        .catch(function (error) {
-          console.log("An error occurred:", error);
-        });
-    };
+    fetch("http://localhost:3001/quote")
+      .then(function (response) {
+        if (response.ok) {
+          return response.json();
+        }
+        throw `${response.status} ${response.statusText}`;
+      })
+      .then(function (quote) {
+        console.log(quote);
+        setQuote(quote);
+      })
+      .catch(function (error) {
+        console.log("An error occurred:", error);
+      });
+  };
 
   useEffect(() => {
     ranQuote();
-  },[isClicked]);
-
+  }, [isClicked]);
+  console.log(quote);
   return (
     <div className="container">
       <div className="row button-row">
--- src/components/Quote/Quote.js
+++ src/components/Quote/Quote.js
@@ -4,13 +4,13 @@ import "./quote.css";
 // function that gets quote prop and prints its object values
 const Quote = ({ quote }) => {
   return (
-    <div className="">
+    <div classname="">
       <h1 className="">
         <i className="medium material-icons">format_quote</i>
-        {quote.text}
+        {quote[0].quote_text}
         <i className="medium material-icons">format_quote</i>
       </h1>
-      <h4 className="author right-align">...{quote.author}</h4>
+      <h4 className="author right-align">...{quote[0].quote_author}</h4>
     </div>
   );
 };
